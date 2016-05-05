c5baa7961af9be1b06af611127117324a2a37c76
Wenderson Ferreira de Souza <wenderson@trustsystems.com.br>
2016-05-05 15:57:03 -0300

    Class base

---
+++ src/main/java/com/designprinciples/lecture3/part1/Player.java
@@ -0,0 +1,19 @@
+package com.designprinciples.lecture3.part1;
+
+public abstract class Player {
+    String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public void train() {
+        System.out.println(name + " trains hard");
+    }
+
+    public void compete() {
+        System.out.println(name + " is very competitive");
+    }
+
+    public abstract void defineTactics();
+}
