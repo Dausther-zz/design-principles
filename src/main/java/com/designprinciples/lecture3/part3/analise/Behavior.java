d7a894c28a036e37f37b39a3841980c8b0ebc35b
Dausther Seidel Perini <dausther@gmail.com>
2016-05-09 12:13:11 -0300

    Removing unnecessary files.

--- src/main/java/com/designprinciples/lecture3/part3/analise/Behavior.java
+++
@@ -1,7 +0,0 @@
-package com.designprinciples.lecture3.part3.analise;
-
-public interface Behavior {
-
-    void run();
-
-}
--- src/main/java/com/designprinciples/lecture3/part3/analise/SoccerPlayerFace.java
+++
@@ -1,7 +0,0 @@
-package com.designprinciples.lecture3.part3.analise;
-
-/**
- * Created by dausther on 06/05/16.
- */
-public interface SoccerPlayerFace {
-}
